04b15b742f65721969fe11ab6983e7e1f38d37a4
serhio <serjdyachenko@gmail.com>
2018-01-10 23:27:55 +0200

    fix gup config

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
 /**
  * Created by user on 10.01.18.
  */
-const gulp = require("gulp")
-const sass = require("gulp-sass")
-const watchSass = require("gulp-watch-sass")
+var gulp = require ('gulp');
 
-gulp.task("sass:watch", () => watchSass([
-    "./public/**/*.{scss,css}",
-    "!./public/libs/**/*"
-])
-    .pipe(sass())
-    .pipe(gulp.dest("./public")));
+//Include plugins
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+
+//Lint Task
+
+gulp.task('lint', function () {
+   return gulp.src('js/*.js')
+       .pipe(jshint())
+       .pipe(jshint.reporter('default'));
+});
+
+//Compile Our Sass
+gulp.task('sass', function(){
+    return gulp.src('scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
+//Concate & minify Js
+gulp.task('scripts', function(){
+   return gulp.src('js/*.js')
+       .pipe(concat('all.js'))
+       .pipe(gulp.dest('dist/js'))
+       .pipe(rename('all.min.js'))
+       .pipe(uglify())
+       .pipe(gulp.dest('dist/js'));
+});
+
+//Watch Files For Chfnges
+gulp.task('watch', function(){
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
+})
+
+//Default Task
+
+gulp.task('default',['lint', 'sass', 'scripts', 'watch']);
