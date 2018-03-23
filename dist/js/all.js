d9efd7b6cc76f126a8cb3529e776dcbbf414a12c
serhio <serjdyachenko@gmail.com>
2018-03-23 11:38:17 +0200

    test isotope plugin in portfolio page

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,11 @@
 /**
  * Created by Asus on 10.01.2018.
  */
+
+var $grid = $('.grid').isotope({
+   itemSelector: '.element-item',
+   layoutMode: 'fitRows',
+   getSortData:{
+       name: '.name'
+   }
+});
